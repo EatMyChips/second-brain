c9b668bfa6bd8dbe207ddaca110d7d76b5753708
matt <matthew.j.goodwin.10june@gmail.com>
2025-07-11 23:33:11 +0100

    month switching

--- src/components/todo/calendar/mod.rs
+++ src/components/todo/calendar/mod.rs
@@ -28,16 +28,45 @@ pub fn Calendar(calendar: Signal<Option<Rc<MountedData>>>) -> Element {
 
 #[component]
 fn CalendarObj() -> Element {
+    // get all date props
     let current_week = use_context::<AppState>().current_week;
     let mut selected_week = use_context::<AppState>().selected_week;
     let mut selected_day = use_context::<AppState>().selected_day;
     let current_day = use_context::<AppState>().current_day;
+    let mut selected_month = use_context::<AppState>().selected_month;
+    let current_month = use_context::<AppState>().current_month;
 
-    let active_month_days = use_signal(|| days_in_month(selected_day.read().year(), selected_day.read().month()));
-    let first_day_count = first_day_of_month(selected_day.read().year(), selected_day.read().month());
-    let month_name = selected_day.read().format("%B").to_string();
-    let total_days = active_month_days + first_day_count;
-    let left_over = if total_days < 36 { 35 - total_days + 2 } else { 42 - total_days + 2};
+    let mut active_month_days= use_signal(|| 0);
+    let mut first_day_count= use_signal(|| 0);
+    let mut month_name = use_signal(|| String::new());
+    let mut left_over = use_signal(|| 0);
+    let mut prev_month_days = use_signal(|| 0);
+    let mut row_count = use_signal(|| 0);
+    // Display data
+    let _ = use_resource( move || {
+        // reactive with selected month
+        let selected_month = selected_month.read().clone();
+
+        active_month_days.set(days_in_month(selected_month.clone()));
+        month_name.set(selected_day.read().format("%B").to_string());
+
+        // working out day counts
+        first_day_count.set(first_day_of_month(selected_month.clone()));
+        let total_days = *active_month_days.read() + *first_day_count.read();
+        let remainder = (total_days - 1) % 7;
+        let extra_days = if remainder == 0 { 0 } else { 7 - remainder };
+        left_over.set(if extra_days == 0 { 1 } else {extra_days + 1});
+        let total_cells = total_days + extra_days;
+        row_count.set(total_cells / 7);
+
+        //previous month
+        let prev_month = Month { year: selected_month.year, month: selected_month.month - 1 };
+        prev_month_days.set(days_in_month(prev_month));
+        async move {
+            log::info!("{total_days:?}");
+            log::info!("{:?}", row_count.read());
+        }
+    });
 
     rsx! {
         div {
@@ -45,30 +74,33 @@ fn CalendarObj() -> Element {
             div {
                 class: "month",
                 h3 {
-                    {month_name}
+                    {month_name.read().clone()}
                 }
                 button {
                     onclick: move |_|{
-
+                        let month = selected_month.read().clone();
+                        selected_month.set(decrease_month(month));
                     },
                     "<-"
                 }
                 button {
                     onclick: move |_|{
-
+                        let month = selected_month.read().clone();
+                        selected_month.set(increase_month(month));
                     },
                     "->"
                 }
             }
             div {
                 class: "days",
-                for i in 1..first_day_count {
+                style: "grid-template-rows: repeat({row_count}, 1fr);",
+                for i in (prev_month_days - *first_day_count.read() + 2)..(prev_month_days + 1) {
                     Day { number: i, inactive: true }
                 }
                 for i in 1..(*active_month_days.read() + 1) {
                     Day { number: i, inactive: false }
                 }
-                for i in 1..left_over{
+                for i in 1..*left_over.read(){
                     Day { number: i, inactive: true }
                 }
             }
@@ -88,8 +120,8 @@ fn Day(number: u32, inactive: bool) -> Element {
     }
 }
 
-fn days_in_month(year: i32, month: u32) -> u32 {
-    let next_date = increase_month(Month{year, month});
+fn days_in_month(month: Month) -> u32 {
+    let next_date = increase_month(month);
 
     let first_of_next_month = NaiveDate::from_ymd_opt(next_date.year, next_date.month, 1).unwrap();
     let last_day = first_of_next_month.pred_opt().unwrap();
@@ -97,7 +129,7 @@ fn days_in_month(year: i32, month: u32) -> u32 {
     last_day.day()
 }
 
-fn increase_month(mut month: Month) -> Month{
+fn increase_month(month: Month) -> Month{
     match month.month {
         12 => {
             Month { year: month.year + 1, month: 1 }
@@ -119,8 +151,8 @@ fn decrease_month(mut month: Month) -> Month{
     }
 }
 
-fn first_day_of_month(year: i32, month: u32) -> u32 {
-    let first_day = Local.ymd(year, month, 1);
+fn first_day_of_month(month: Month) -> u32 {
+    let first_day = Local.ymd(month.year, month.month, 1);
     weekday_to_number(first_day.weekday())
 }
 
